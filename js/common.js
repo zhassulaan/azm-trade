637b1fe2cd9ba6d567853ef82cf3147d232128f8
Zhassulan <190103383@stu.sdu.edu.kz>
2024-02-27 02:57:21 +0600

    home page - partners sections

--- js/common.js
+++ js/common.js
@@ -11,3 +11,33 @@ function handleLike(id) {
 	else
 		card.classList.add('active');
 }
+
+function dragSlider(id) {
+	const container = $g(`#${id}`);
+	let isDown = false;
+	let startX;
+	let scrollLeft;
+
+	container.addEventListener('mousedown', (e) => {
+		isDown = true;
+		container.classList.add('active');
+		startX = e.pageX - container.offsetLeft;
+		scrollLeft = container.scrollLeft;
+	});
+	container.addEventListener('mouseleave', () => {
+		isDown = false;
+		container.classList.remove('active');
+	});
+	container.addEventListener('mouseup', () => {
+		isDown = false;
+		container.classList.remove('active');
+	});
+	container.addEventListener('mousemove', (e) => {
+		if (!isDown)
+			return;
+		e.preventDefault();
+		const x = e.pageX - container.offsetLeft;
+		const walk = (x - startX) * 3;
+		container.scrollLeft = scrollLeft - walk;
+	});
+}
